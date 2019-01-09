7bd24d7c5082a00bdfa74c3708670ae3715e4894
tech-a47 <tech-a47@Tech-A47s-MacBook-Air.local>
2019-01-09 09:05:56 +0000

    initial commit

---
+++ js/apps.js
@@ -0,0 +1,305 @@
+console.log("Loaded");
+
+var square = document.getElementsByTagName("td");
+var turn_num = 0;
+var floor_count = 1;
+var time = 0
+setup();
+clock();
+var position = document.getElementsByClassName("position")[0]
+
+
+// To do:
+// add timer
+
+//score system
+
+//add a start/reset button
+//add highscore.
+up.addEventListener("click", function(event){
+  move_up();
+});
+
+down.addEventListener("click", function(event){
+  move_down();
+});
+
+left.addEventListener("click", function(event){
+  move_left();
+});
+
+right.addEventListener("click", function(event){
+  move_right();
+});
+
+
+function move_up() {
+  for (var i = 0; i < square.length; i++) {
+    tile = square[i]
+    if ((tile == position)&&(i<=9)) {
+      target = square[i+90];
+      if (target.innerHTML == "") {
+        new_location = target
+        move(new_location);
+      }
+      else if (target.innerHTML == "#") {
+        damage(target);
+        new_location = position;
+      }
+      else if (target.innerHTML == "E") {
+        exit();
+        new_location = document.getElementsByClassName("position")[0];
+      }
+    }
+    else if (tile == position) {
+      target = square[i-10];
+      if (target.innerHTML == "") {
+        new_location = target;
+        move(new_location);
+      }
+      else if (target.innerHTML == "#") {
+        damage(target);
+        new_location = position;
+      }
+      else if (target.innerHTML == "E") {
+        exit();
+        new_location = document.getElementsByClassName("position")[0];
+      }
+    }
+  }
+  position = new_location
+  endturn();
+};
+
+function move_down(){
+  for (var i = 0; i < square.length; i++) {
+    tile = square[i]
+    if ((tile == position)&&( i>= 90)) {
+      target = square[i-90]
+      if (target.innerHTML == "") {
+        new_location = target
+        move(new_location);
+      }
+      else if (target.innerHTML == "#") {
+        damage(target);
+        new_location = position;
+      }
+      else if (target.innerHTML == "E") {
+        exit();
+        new_location = document.getElementsByClassName("position")[0];
+      }
+    }
+    else if (tile == position) {
+      target = square[i+10]
+      if (target.innerHTML == "") {
+        new_location = target
+        move(new_location);
+      }
+      else if (target.innerHTML == "#") {
+        damage(target);
+        new_location = position;
+      }
+      else if (target.innerHTML == "E") {
+        exit();
+        new_location = document.getElementsByClassName("position")[0];
+      }
+    }
+  }
+  position = new_location;
+  endturn();
+};
+
+function move_left(){
+  for (var i = 0; i < square.length; i++) {
+    tile = square[i]
+    if ((tile == position) && (i % 10 == 0)) {
+      target = square[i+9]
+      if (target.innerHTML == "") {
+        new_location = target
+        move(new_location);
+      }
+      else if (target.innerHTML == "#") {
+        damage(target);
+        new_location = position;
+      }
+      else if (target.innerHTML == "E") {
+        exit();
+        new_location = document.getElementsByClassName("position")[0];
+      }
+    }
+    else if (tile == position) {
+      target = square[i-1]
+      if (target.innerHTML == "") {
+        new_location = target;
+        move(new_location, i);
+      }
+      else if (target.innerHTML == "#") {
+        damage(target);
+        new_location = position;
+      }
+      else if (target.innerHTML == "E") {
+        exit();
+        new_location = document.getElementsByClassName("position")[0];
+      }
+    }
+  }
+  position = new_location;
+  endturn();
+};
+
+function move_right(){
+  for (var i = 0; i < square.length; i++) {
+    tile = square[i]
+    if ((tile == position) && (((i % 10)-9) == 0)) {
+      target = square[i-9]
+      if (target.innerHTML == "") {
+        new_location = target
+        move(new_location, i);
+      }
+      else if (target.innerHTML == "#") {
+        damage(target);
+        new_location = position;
+      }
+      else if (target.innerHTML == "E") {
+        exit();
+        new_location = document.getElementsByClassName("position")[0];
+      }
+    }
+    else if (tile == position) {
+      target = square[i+1]
+      if (target.innerHTML == "") {
+        new_location = target
+        move(new_location, i);
+      }
+      else if (target.innerHTML == "#") {
+        damage(target);
+        new_location = position;
+      }
+      else if (target.innerHTML == "E") {
+        exit();
+        new_location = document.getElementsByClassName("position")[0];
+      }
+    }
+  }
+  position = new_location;
+  endturn();
+};
+
+function move(new_location){
+    new_location.classList.add("position");
+    position.classList.remove("position");
+    position.innerHTML = "";
+    new_location.innerHTML = "@";
+}
+
+function endturn(){
+  turn_num = (turn_num + 1)
+
+  document.getElementById("turn-num").innerHTML = turn_num;
+}
+
+
+function setup(){
+  for (var i = 0; i < 60; i++) {
+    target = Math.floor(Math.random() * 100)
+    new_wall = square[target]
+    new_wall.classList.add("wall");
+    new_wall.classList.add("health-3");
+    new_wall.innerHTML = "#";
+  }
+
+  start_square = Math.floor(Math.random() * 100)
+  start_check = start_square
+  start_pos = square[start_square]
+  start_pos.classList.remove("wall");
+  start_pos.classList.remove("health-3");
+  start_pos.classList.add("position");
+  start_pos.innerHTML = "@";
+
+
+
+  start_square = Math.floor(Math.random() * 50)
+    if(start_check < 50){
+      start_stairs = square[(start_square + 50)]
+    }
+    else if (start_check <100) {
+      start_stairs = square[start_square]
+
+    }
+  start_stairs.classList.remove("wall");
+  start_stairs.classList.remove("health-3");
+  start_stairs.classList.add("exit");
+  start_stairs.innerHTML = "E";
+
+}
+
+function clear(){
+  for (var i = 0; i < square.length; i++){
+  tile = square[i]
+
+  tile.classList.remove("wall", "health-3", "health-2", "health-1", "exit", "position");
+  tile.innerHTML = "";}
+}
+
+function damage(target){
+  health = target.classList[1]
+
+  switch (health) {
+    case "health-3":
+      console.log(health);
+      target.classList.remove("health-3");
+      target.classList.add("health-2");
+      break;
+    case "health-2":
+      console.log(health);
+      target.classList.remove("health-2");
+      target.classList.add("health-1");
+      break;
+    case "health-1":
+      console.log(health);
+      target.classList.remove("health-1");
+      target.classList.remove("wall");
+      target.innerHTML = "";
+      break;
+    default:
+
+  }
+}
+
+function exit (){
+  clear();
+  setup();
+  floor_count = (floor_count + 1)
+  document.getElementById("floor_count").innerHTML = floor_count;
+}
+
+
+function  clock(){
+  setInterval(function(){
+    time++;
+    document.getElementById("time").innerHTML= (time )
+  }, 1000);
+}
+
+
+//Arrow key functionality
+
+$(document).keydown(function(e){
+  switch (e.which) {
+    case 37:
+      move_left();
+      break;
+    case 38:
+      move_up();
+      break;
+    case 39:
+      move_right();
+      break;
+    case 40:
+      move_down();
+      break;
+    default:
+  }
+
+  e.preventDefault();
+});
